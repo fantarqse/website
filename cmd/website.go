d7c03062f83a8659b28b672cdb27b1597193638a
fantarqse <fantarose51@gmail.com>
2024-04-18 20:55:25 +0300

    feat: added listen and serve

--- cmd/website.go
+++ cmd/website.go
@@ -1,10 +1,19 @@
 package website
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const port string = ":8888"
 
 func Run() {
-	fmt.Println("Starting website...")
-	fmt.Println("Not implemented yet!")
+	// TODO: add static file
+	// TODO: init router
+	log.Printf("Starting server on %s\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("the server failed to start: %s", err)
+	}
 
 	// TODO: Graceful shutdown
 }
